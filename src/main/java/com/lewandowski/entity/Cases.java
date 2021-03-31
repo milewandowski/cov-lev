a7ce4d3661bf69de0e0ee259e8f91cda9e56a970
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-31 23:00:32 +0200

    Create Cases and Deaths entities

---
+++ src/main/java/com/lewandowski/entity/Cases.java
@@ -0,0 +1,64 @@
+package com.lewandowski.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cases {
+
+    @JsonProperty("new")
+    private String newCases;
+    private Integer active;
+    private Integer critical;
+    private Integer recovered;
+    private Integer total;
+
+    public String getNewCases() {
+        return newCases;
+    }
+
+    public void setNewCases(String newCases) {
+        this.newCases = newCases;
+    }
+
+    public Integer getActive() {
+        return active;
+    }
+
+    public void setActive(Integer active) {
+        this.active = active;
+    }
+
+    public Integer getCritical() {
+        return critical;
+    }
+
+    public void setCritical(Integer critical) {
+        this.critical = critical;
+    }
+
+    public Integer getRecovered() {
+        return recovered;
+    }
+
+    public void setRecovered(Integer recovered) {
+        this.recovered = recovered;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Cases{" +
+                "newCases='" + newCases + '\'' +
+                ", active=" + active +
+                ", critical=" + critical +
+                ", recovered=" + recovered +
+                ", total=" + total +
+                '}';
+    }
+}
---
+++ src/main/java/com/lewandowski/entity/Deaths.java
@@ -0,0 +1,34 @@
+package com.lewandowski.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Deaths {
+
+    @JsonProperty("new")
+    private String newDeaths;
+    private Integer total;
+
+    public String getNewDeaths() {
+        return newDeaths;
+    }
+
+    public void setNewDeaths(String newDeaths) {
+        this.newDeaths = newDeaths;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Deaths{" +
+                "newDeaths='" + newDeaths + '\'' +
+                ", total=" + total +
+                '}';
+    }
+}
