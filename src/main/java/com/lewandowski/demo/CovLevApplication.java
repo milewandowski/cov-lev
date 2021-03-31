6dd879874c89c2f583b12156fd2c84c829eba2b7
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-03-31 12:04:09 +0200

    Initial commit

---
+++ src/main/java/com/lewandowski/demo/CovLevApplication.java
@@ -0,0 +1,13 @@
+package com.lewandowski.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovLevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovLevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lewandowski/demo/CovLevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lewandowski.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovLevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
