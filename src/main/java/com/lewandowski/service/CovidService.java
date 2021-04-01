2f4c18a58f4f7a87ef5e612e55368373ea2c3cd1
Michal Lewandowski <mlewandowskimichal@gmail.com>
2021-04-01 23:18:43 +0200

    Fix code to use reactive programming and create BotConfig

--- src/main/java/com/lewandowski/service/CovidService.java
+++ src/main/java/com/lewandowski/service/CovidService.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @PropertySource("classpath:APIKeys.properties")
@@ -19,43 +22,37 @@ public class CovidService {
     private String apiKey;
     private final WebClient.Builder webClientBuilder;
     private final String BASE_URI = "https://covid-193.p.rapidapi.com";
-    private CountriesResponse countriesResponse;
+    private final List<String> countries = new ArrayList<>();
 
     public CovidService(WebClient.Builder webClientBuilder) {
         this.webClientBuilder = webClientBuilder;
     }
 
     @PostConstruct
-    public CountriesResponse getAllCountries() {
-        if(countriesResponse == null) {
-            countriesResponse = webClientBuilder
-                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .defaultHeader("x-rapidapi-key", apiKey)
-                    .build()
-                    .get()
-                    .uri(BASE_URI + "/countries")
-                    .retrieve()
-                    .bodyToMono(CountriesResponse.class)
-                    .block();
-        }
-        return countriesResponse;
+    public void getAllCountries() {
+        webClientBuilder
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader("x-rapidapi-key", apiKey)
+                .build()
+                .get()
+                .uri(BASE_URI + "/countries")
+                .retrieve()
+                .bodyToMono(CountriesResponse.class)
+                .subscribe(countriesResponse -> countries.addAll(countriesResponse.getResponse()));
     }
 
     public void getStatisticsForCountry(String countryName) {
-        boolean isPresent = getAllCountries().getResponse().stream()
+        boolean isPresent = countries.stream()
                 .anyMatch(country -> country.equals(countryName));
-        if(isPresent) {
-            StatisticsResponse statisticsResponse = webClientBuilder
+        if (isPresent) {
+            Mono<StatisticsResponse> statisticsResponse = webClientBuilder
                     .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                     .defaultHeader("x-rapidapi-key", apiKey)
                     .build()
                     .get()
                     .uri(BASE_URI + "/statistics?country=" + countryName)
                     .retrieve()
-                    .bodyToMono(StatisticsResponse.class)
-                    .block();
-
-            System.out.println(statisticsResponse);
+                    .bodyToMono(StatisticsResponse.class);
         }
     }
 
